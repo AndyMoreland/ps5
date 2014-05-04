6dfa9bd036e51f23f2f29d3dfc40f731e2857ec1
Andrew Moreland <andy@andymoreland.com>
2014-05-04 02:13:08 -0700

    Most of splay tree done
    Still need to implement splay tree constructor

--- src/SplayTree.java
+++ src/SplayTree.java
@@ -2,6 +2,8 @@
  * An implementation of a BST backed by a splay tree.
  */
 public class SplayTree implements BST {
+    private TreeNode root;
+
     /**
      * Constructs a new tree from the specified array of weights. Since splay
      * trees don't care about access probabilities, you should only need
@@ -23,7 +25,119 @@ public class SplayTree implements BST {
      * @return Whether it's in the BST.
      */
     public boolean contains(int key) {
-        // TODO: Implement this!
-        return false;
+        TreeNode node;
+
+        for (node = root; node != null && node.getKey() != key; ) {
+            if (key < node.getKey()) {
+                node = node.getLeft();
+            } else {
+                node = node.getRight();
+            }
+        }
+
+        if (node == null) {
+            return false;
+        }
+
+        while (root != node) {
+            node.splay();
+        }
+
+        return true;
+    }
+
+    private class TreeNode {
+        private int key;
+        private double weight;
+
+        private TreeNode left;
+        private TreeNode right;
+        private TreeNode parent;
+
+        public TreeNode(TreeNode parent, int key, double weight) {
+            this.parent = parent;
+            this.key = key;
+            this.weight = weight;
+        }
+
+        public void splay() {
+            assert(parent != null);
+            TreeNode grandFather = getGrandfather();
+
+            if (getGrandfather() == null) {
+                // zig
+                this.rotate(parent);
+            } else if (getGrandfather().getRight() == parent) {
+                // zig-zag
+                this.rotate(parent);
+                // The above line makes the original grandfather the parent.
+                this.rotate(grandFather);
+            } else {
+                // zig-zig
+                parent.rotate(grandFather);
+                this.rotate(parent);
+            }
+        }
+
+        /* Properly detects whether or not we need left/right rotation */
+        private void rotate(TreeNode parent) {
+            if (parent == root) {
+                root = this;
+            }
+
+            if (parent.getLeft() == this) {
+                /* We're doing a right-rotation */
+                parent.setLeft(this.getRight());
+                this.setRight(parent);
+            } else {
+                parent.setRight(this.getLeft());
+                this.setLeft(parent);
+            }
+        }
+
+        public TreeNode getGrandfather() {
+            assert (parent != null);
+            return parent.getParent();
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public void setKey(int key) {
+            this.key = key;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
+
+        public void setWeight(double weight) {
+            this.weight = weight;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+
+        public TreeNode getParent() {
+            return parent;
+        }
+
+        public void setParent(TreeNode parent) {
+            this.parent = parent;
+        }
     }
 }
